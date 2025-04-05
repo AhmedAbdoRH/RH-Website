7c921b83a681993e29b184067d3dd45db1cb1c8b
Ahmd Abdo <rehlathadaf@gmail.com>
2025-04-05 17:41:37 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // لا تحتاج عادةً إلى assetsInclude للصور المستوردة في المكونات
-  // assetsInclude: ['src/assets/logo.png'],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  assetsInclude: ['src/logo.png'],
 });
