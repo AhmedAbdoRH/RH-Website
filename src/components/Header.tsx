bfc5540864f9241ce3e0046fab0b12dafc5169e1
Ahmd Abdo <rehlathadaf@gmail.com>
2025-04-05 17:14:42 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import logo from './scr/logo.png'; // Import the logo from the scr folder
 
 export default function Header() {
   return (
     <header className="bg-white shadow-lg">
       <div className="container mx-auto px-4 py-4 flex items-center justify-between">
-        <img 
-          src="/logo.png" 
-          alt="Logo" 
+        <img
+          src={logo} // Use the imported logo variable
+          alt="Logo"
           className="h-12 w-12"
         />
         <nav>
